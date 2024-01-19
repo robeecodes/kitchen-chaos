3af69fe412d4afd54120532e2e6d5744bf689711
Robin <97387960+robeecodes@users.noreply.github.com>
2024-01-19 11:43:41 +0000

    Create Collision

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,24 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GameInput : MonoBehaviour
-{
+public class GameInput : MonoBehaviour {
+    private PlayerInputActions _playerInputActions;
+    
+    private void Awake() {
+        _playerInputActions = new PlayerInputActions();
+        _playerInputActions.Player.Enable();
+    }
+
     public Vector2 GetMovementVectorNormalised() {
-        Vector2 inputVector = new Vector2(0, 0);
-        
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y++;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y--;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x--;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x++;
-        }
+        Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
 
         inputVector = inputVector.normalized;
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,13 @@
-using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour {
 
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+
+    private const float PlayerRadius = .7f;
+    private const float PlayerHeight = 2f;
 
     private bool _isWalking;
     
@@ -14,7 +17,31 @@ public class Player : MonoBehaviour {
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         _isWalking = moveDir != Vector3.zero;
 
-        transform.position += moveDir * (moveSpeed * Time.deltaTime);
+        float moveDist = moveSpeed * Time.deltaTime;
+        
+        bool canMove = CanMove(moveDir, moveDist);
+
+        if (!canMove) {
+            // Check if movement along the x-axis is possible
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = CanMove(moveDirX, moveDist);
+
+            if (canMove) {
+                moveDir = moveDirX;
+            }
+            else {
+                // Check if movement along the z-axis is possible
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = CanMove(moveDirZ, moveDist);
+                
+                if (canMove) {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+        if (canMove) {
+            transform.position += moveDir * (moveDist);
+        }
 
         float rotSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotSpeed * Time.deltaTime);
@@ -22,5 +49,9 @@ public class Player : MonoBehaviour {
 
     public bool IsWalking() {
         return _isWalking;
+    }
+
+    private bool CanMove(Vector3 moveDir, float moveDist) {
+        return !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * PlayerHeight, PlayerRadius,  moveDir, moveDist);
     }
 }
